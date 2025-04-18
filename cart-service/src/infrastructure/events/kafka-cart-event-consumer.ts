dfb1cb942eac29aa3c4ecf22feea4e57cd3f7069
andresdvx <andresdavidpachecocuadro@gmail.com>
2025-04-18 10:56:41 -0500

    fix: kafka messaage format

--- cart-service/src/infrastructure/events/kafka-cart-event-consumer.ts
+++ cart-service/src/infrastructure/events/kafka-cart-event-consumer.ts
@@ -1,38 +1,54 @@
-
-
-import { Consumer, Kafka, Producer } from 'kafkajs';
+import { Consumer, Kafka, Producer } from "kafkajs";
 
 export class KafkaCartConsumer {
+  private readonly kafkaConfig = {
+    clientId: "cart-service",
+    brokers: ["localhost:9092"],
+  };
+  private producer: Producer;
+  private consumer: Consumer;
 
-    private readonly kafkaConfig = {
-        clientId: 'cart-service',
-        brokers: ['localhost:9092'],
-    };
-    private producer: Producer;
-    private consumer: Consumer;
+  constructor() {
+    const kafka = new Kafka(this.kafkaConfig);
+    this.producer = kafka.producer();
+    this.consumer = kafka.consumer({ groupId: "cart-service-group" });
+  }
 
-    constructor() {
-        const kafka = new Kafka(this.kafkaConfig);
-        this.producer = kafka.producer();
-        this.consumer = kafka.consumer({ groupId: 'cart-service-group' });
-    }
+  async connect(): Promise<void> {
+    await this.producer.connect();
+  }
 
-    async connect(): Promise<void> {
-        await this.producer.connect();
-    }
-
-    async publishCartRemoved(userId: string, productId: string, productName: string): Promise<void> {
-        await this.connect();
-        await this.producer.send({
-            topic: 'notifications',
-            messages: [{
-                value: JSON.stringify({
-                    userId: userId,
-                    productId: productId,
-                    timestamp: new Date().toISOString(),
-                    product: { name: productName },
-                })
-            }]
-        });
-    }
+  async publishCartRemoved(
+    userId: string,
+    productId: string,
+    productName: string
+  ): Promise<void> {
+    await this.connect();
+    await this.producer.send({
+      topic: "notifications",
+      messages: [
+        {
+          value: JSON.stringify({
+            timeStamp: new Date().toLocaleDateString(),
+            source: "CardUpdated",
+            topic: "card-updates",
+            originTopic: "cart-removals",
+            payload: {
+              user: {
+                name: "Jimmy",
+                email: "",
+              },
+              productId: productId,
+              timestamp: new Date().toISOString(),
+              product: { name: productName },
+            },
+            snapShot: {
+              productId,
+              userId,
+            },
+          }),
+        },
+      ],
+    });
+  }
 }
